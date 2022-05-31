1da91ec6fe1159ea76f63a89fe520223cad05470
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-05-31 17:17:25 +0300

    up to task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for n in range(1, 101):
         if n % 15 == 0:
-            print("Fizzbuzz", end=" ")
+            print("FizzBuzz", end=" ")
             continue
         elif n % 3 == 0:
             print("Fizz", end=" ")
