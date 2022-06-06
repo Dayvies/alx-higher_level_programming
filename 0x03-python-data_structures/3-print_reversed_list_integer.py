1c48c9b8358a371b353db106b5742b504fc15925
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-06-06 13:32:32 +0300

    Mandatory Debug Python

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     z = len(my_list)
-    if z > 1:
+    if my_list:
         my_list.reverse()
-    for i in my_list:
-        print("{:d}".format(i))
+        for i in my_list:
+            print("{:d}".format(i))
