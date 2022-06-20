613994a2dc9c1261241aa15a6e10636b18d172db
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-06-20 12:32:28 +0300

    0 and 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
+    if not my_list:
+        return count
     for n in my_list:
         try:
             print(n, end='')
