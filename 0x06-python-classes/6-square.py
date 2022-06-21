a558a250419a08c104e1cc00ae55d1a7315db446
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-06-21 22:27:16 +0300

    Mandatory Debug

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -92,9 +92,11 @@ class Square:
                 __position
                 i
         """
+        if (self.__size == 0):
+            print()
+            return
         for i in range(self.__position[1]):
             print()
         for i in range(self.__size):
             print("{}{}".format(' '*self.__position[0], '#'*self.__size))
-        if (self.__size == 0):
-            print()
+        
