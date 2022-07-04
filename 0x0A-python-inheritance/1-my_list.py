b7375e6cb746fcc15f0de11adb63390711c0c132
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-07-04 16:31:51 +0300

    Upto5

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Has two classes . One inherits from the other
+"""
+class MyList(list):
+        """
+        Inherits properties from list
+        """
+        def print_sorted(self):
+                """
+                returns a sorted list
+                """
+                print(sorted(self))
