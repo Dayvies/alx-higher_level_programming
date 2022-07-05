6142b9e75b1b05493126abdaf6a70f81b1d93fd6
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-07-05 18:25:33 +0300

    Mandatory

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 def main():
+    """Main function of module"""
     try:
         my_list = load_from_json_file("add_item.json")
     except:
@@ -14,5 +15,6 @@ def main():
         my_list.append(sys.argv[i])
     save_to_json_file(my_list, "add_item.json")
 
+
 if __name__ == "__main__":
-        main()
+    main()
