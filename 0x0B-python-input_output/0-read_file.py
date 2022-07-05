9024c6f2b7471f239af2fa5157211e8b0a041665
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-07-05 18:14:50 +0300

    Mandatory

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that reads files"""
+
+
+def read_file(filename=""):
+    """Reads all contexts of a file and prints"""
+    with open(filename, encoding="utf-8") as fil:
+        print(fil.read())
---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Student class to dict"""
+
+
+class Student():
+    """A class of students"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes class student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Obtain json representation of class"""
+        dict2 = self.__dict__.copy()
+        if attrs == None or len(attrs) < 1:
+            return dict2
+        for key in attrs:
+                if (type(key) != str):
+                        return dict2
+        delKey =[]
+        for key in dict2.keys():
+            if key in attrs:
+                pass
+            else:
+                delKey.append(key)
+        for key in delKey:
+                del dict2[key]
+        return dict2
