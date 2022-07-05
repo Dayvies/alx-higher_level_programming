bfbe1778eae0b3949409c56ae8cf940edaf4c901
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-07-05 11:24:49 +0300

    Equality

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-"""
-Interchanges equal sign of int
+"""Module 100-my_int.
+Creates a class that inherits from int.
 """
 
 
 class MyInt(int):
+    """Class inheriting from int,
+    But reverses the behavior of != and ==.
+    """
+
     def __eq__(self, other):
-        """
-        not equal function returns true
-        """
+        """Equality becomes inequality."""
+
         return super().__ne__(other)
 
     def __ne__(self, other):
-        """
-        equal function returns true
-        """
+        """Inequality becomes equality."""
+
         return super().__eq__(other)
+        
