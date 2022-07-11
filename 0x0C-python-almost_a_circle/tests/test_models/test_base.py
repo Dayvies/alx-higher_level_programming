ec0bf5fbabe71c246da1a6fe3166fcd5189820c5
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-07-11 19:28:20 +0300

    Debug2

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -14,11 +14,33 @@ class BaseTest(unittest.TestCase):
     b2 = Base(56)
     b3 = Base()
     b4 = Base()
+    bt = Base()
 
     def test_objectsid(self):
         """test id allocation"""
         self.assertEqual(BaseTest.b4.id, 3)
         self.assertEqual(BaseTest.b2.id, 56)
+    def test_1_0(self):
+        """Create new instances: check for id."""
+        
+        self.assertEqual(BaseTest.bt.id, 4)
+        b2 = Base(12)
+        self.assertEqual(b2.id, 12)
+        b3 = Base(0)
+        self.assertEqual(b3.id, 0)
+        b4 = Base(927)
+        self.assertEqual(b4.id, 927)
+        b5 = Base(-5)
+        self.assertEqual(b5.id, -5)
+        b6 = Base(9)
+        self.assertEqual(b6.id, 9)
+
+    def test_1_1(self):
+        """Test for type and instance."""
+
+        b6 = Base()
+        self.assertEqual(type(b6), Base)
+        self.assertTrue(isinstance(b6, Base))
 
     def test_15(self):
         """testing json"""
