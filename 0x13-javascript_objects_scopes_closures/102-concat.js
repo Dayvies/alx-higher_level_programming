18988acf70b70732753bfabb596e2c858f664e92
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-09-06 22:28:48 +0300

    final task

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,6 +6,5 @@ const fs = require('fs');
 const data = fs.readFileSync(args[2], 'utf8');
 const data2 = fs.readFileSync(args[3], 'utf8');
 
-
 fs.writeFileSync(args[4], data);
 fs.appendFileSync(args[4], data2);
