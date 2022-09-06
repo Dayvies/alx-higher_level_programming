a30172b01fb577df6324a2c1843f305282c32045
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-09-06 15:15:47 +0300

    js debug

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, search) {
-        let count = 0;
-        for (let i = 0; i < list.length; i++) {
-          if (search === list[i]) { count += 1; }
-        }
-        return count;
-      };
-      
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (search === list[i]) { count += 1; }
+  }
+  return count;
+};
