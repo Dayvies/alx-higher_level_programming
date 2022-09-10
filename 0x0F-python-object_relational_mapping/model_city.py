b31aac9f7bf24c8b06795fdaeb8d4f2fe7b6af00
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-09-10 21:25:24 +0300

    Mandatory debug

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,7 +4,9 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City (Base):
+    """city definition"""
     __tablename__ = 'cities'
     id = Column(Integer, autoincrement=True,
                 primary_key=True, nullable=False, unique=True)
