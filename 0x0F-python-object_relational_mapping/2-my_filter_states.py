bb0a7ba086a52c857c04bc3b5e21daa1db9a28ae
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-09-10 21:15:21 +0300

    Mandatory debug

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+"""script that takes in an argument and displays all values"""
 import MySQLdb
 import sys
 
 
 def main():
+    """main function"""
     arg = sys.argv
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=arg[1],
                            passwd=arg[2], db=arg[3], charset="utf8")
     cur = conn.cursor()
-    statement = "SELECT * FROM states WHERE name like '{}' ORDER BY id ASC".format(
+    statement = "SELECT * FROM states WHERE name like BINARY\
+        '{}' ORDER BY id ASC".format(
         arg[4])
     cur.execute(statement)
     query_rows = cur.fetchall()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""module for declaring state"""
 
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
 Base = declarative_base()
 
 
 class State (Base):
+    """class and table definition"""
     __tablename__ = 'states'
     id = Column(Integer, autoincrement=True,
                 primary_key=True, nullable=False, unique=True)
