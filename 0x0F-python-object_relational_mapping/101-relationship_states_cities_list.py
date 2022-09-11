6fe73e4a48b98c0aceaf63e280e773aea5fcd076
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-09-11 14:49:46 +0300

    advanced 2,3

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -14,6 +14,7 @@ def main():
     """main function"""
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     for state in session.query(State).order_by(State.id):
