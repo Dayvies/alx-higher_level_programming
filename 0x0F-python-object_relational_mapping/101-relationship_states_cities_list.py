ea4b89598f1034a55b728c8bcffcedd6116aea75
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-09-11 14:47:55 +0300

    advanced 2,3

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -16,10 +16,10 @@ def main():
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id):
-        print("{}: {}".format(instance.id, instance.name))
-        for cit in instance.cities:
-            print("    {}: {}".format(cit.id, cit.name))
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
 
 
 if __name__ == "__main__":
