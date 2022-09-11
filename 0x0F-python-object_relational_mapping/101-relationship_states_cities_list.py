26cdf80cf56c709ad0084cc7694af6b2ca0bb472
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-09-11 14:56:25 +0300

    advanced 2,3

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-"""script that lists all State objects from the database hbtn_0e_6_usa"""
-
-from sqlalchemy.orm import sessionmaker
-
+"""
+Lists all States and corresponding Cities in the database hbtn_0e_101_usa.
+"""
 import sys
-from relationship_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import State
 from relationship_city import City
 
-from sqlalchemy import (create_engine)
-
-
-def main():
-    """main function"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).order_by(State.id).all():
+
+    for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
             print("    {}: {}".format(city.id, city.name))
-
-
-if __name__ == "__main__":
-    main()
