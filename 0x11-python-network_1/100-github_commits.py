c436e6441eaeb15806a9126cf1769c386159ff6a
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-09-24 02:17:06 +0300

    ruby on commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""headers using requests"""
+import requests
+import sys
+
+
+def main():
+    """main function"""
+
+    user = 'https://api.github.com/repos/{}/{}/commits'.format(
+        sys.argv[1], sys.argv[2])
+    login = requests.get(user)
+    x = login.json()
+    count = 0
+    for instance in x:
+        if count == 10:
+            return
+        commit = instance.get('commit')
+        commiter = commit.get('author').get('name')
+        sha = instance.get('sha')
+        print('{}: {}'.format(sha, commiter))
+        count += 1
+
+
+if __name__ == "__main__":
+    main()
