32c4b42a604e75c57ba2122569db91b61b3edba5
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-10-18 14:58:21 +0300

    debug task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,15 +4,16 @@ const request = require('request');
 const args = process.argv;
 
 const url = args[2];
+const id = 18;
 request(url, function (error, response, body) {
   const content = JSON.parse(body);
   if (error) { console.log(error); }
   let count = 0;
   content.results.forEach(function (item) {
     const z = item.characters;
-    if (z.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-      count += 1;
-    }
+    z.forEach(function (item) {
+      if (item.includes(id)) { count++; }
+    });
   });
   console.log(count);
 });
