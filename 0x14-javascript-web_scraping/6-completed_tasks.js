935fef3bd2ac9d89abeb0faef766cf284e7b4816
Dayvies <momanyi.davies@students.jkuat.ac.ke>
2022-10-18 15:28:36 +0300

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,22 +1,17 @@
 #!/usr/bin/node
 const request = require('request');
 
-
 const url = process.argv[2];
-let dict = {}
+const dict = {};
 request(url, (error, response, body) => {
   if (error) {
     console.log(error);
   }
-  let content = JSON.parse(body);
-  content.forEach(function (item){
-        if (item.completed == true)
-        {
-                if (item.userId in dict)
-                        dict[item.userId] += 1;
-                else
-                        dict[item.userId] = 1;
-        }
+  const content = JSON.parse(body);
+  content.forEach(function (item) {
+    if (item.completed === true) {
+      if (item.userId in dict) { dict[item.userId] += 1; } else { dict[item.userId] = 1; }
+    }
   });
-  console.log(dict)
+  console.log(dict);
 });
